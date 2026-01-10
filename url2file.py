fb2e21ea9952ab4e3fc49445452b1957f92624c6
OneManArmy <mister228orange@gmail.com>
2026-01-11 04:20:39 +0500

    fix up single file creating

--- url2file.py
+++ url2file.py
@@ -2,9 +2,20 @@ import docker
 
 
 def url2singlefile(url:str) -> str:
-    image = docker.APIClient.pull("capsulecode", "singlefile")
-    container = docker.api.APIClient.build(image)
-    html = docker.APIClient.start(container=container)
-    return html
+    client = docker.from_env()  # Create a client instance from environment variables
+    
+    try:
+        image = client.images.pull("capsulecode/singlefile")  # Pull the required Docker image
+        
+        container = client.containers.run(image, url)  # Run the image in a new container
+        
+        html_content = container.decode() # Get HTML content from the running container
+    except docker.errors.APIError as e:
+        print(f"Docker error: {e}")
+        raise "Downloading error"
+    
+    return html_content
 
-url2singlefile("vk.com")
+
+file = url2singlefile("https://oneman4rmy.carrd.co")
+print(file)
